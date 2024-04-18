c9dce5fab2a53cda2145b42e14f419f98efc1e17
Ginevra Testa <ginevra99testaa@gmail.com>
2024-04-18 19:16:00 +0200

    signup-page-logic-implemented

--- views/src/Components/loginPage/LoginPage.jsx
+++ views/src/Components/loginPage/LoginPage.jsx
@@ -1,104 +1,154 @@
-import { Box, Typography, TextField, Grid, Container, Button } from "@mui/material";
-import "./LoginPage.css";
+import {
+    Box,
+    Typography,
+    TextField,
+    Grid,
+    Container,
+    Button,
+  } from "@mui/material";
+  import { useState, useEffect } from "react";
+  import axios from "axios";
+  import "./LoginPage.css";
 
-const LoginPage = () => {
-  return (
-    <Container component="main" maxWidth="xs" display="flex">
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          py: "24px",
-        }}
-      >
-        <Typography variant="h4" m="0" pb="16px" color="#A4D8E7">
-          Sign up
-        </Typography>
+  const LoginPage = () => {
+    const [formData, setFormData] = useState({
+      name: "",
+      surname: "",
+      email: "",
+      dateOfBirth: "",
+      occupation: "",
+      password: "",
+    });
 
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        const response = await axios.post(
+          "http://localhost:3023/signup",
+          formData
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error creating user:", error);
+      }
+    };
+
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+
+    useEffect(() => {
+    }, [formData]);
+
+    return (
+      <Container component="main" maxWidth="xs" display="flex">
         <Box
-          component="form"
           sx={{
-            border: 2,
-            borderColor: "#A4D8E7",
-            borderRadius: "16px",
-            boxShadow: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            py: "24px",
           }}
-          p="32px"
         >
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-              fullWidth
-                required
-                id="name"
-                label="Name"
-                autoComplete="first-name"
-                autoFocus
-              />
+          <Typography variant="h4" m="0" pb="16px" color="#A4D8E7">
+            Sign up
+          </Typography>
+
+          <Box
+            onSubmit={handleSubmit}
+            component="form"
+            sx={{
+              border: 2,
+              borderColor: "#A4D8E7",
+              borderRadius: "16px",
+              boxShadow: 3,
+            }}
+            p="32px"
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  required
+                  id="name"
+                  label="Name"
+                  autoComplete="first-name"
+                  autoFocus
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  required
+                  id="surname"
+                  label="Surname"
+                  autoComplete="family-name"
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  required
+                  id="email"
+                  label="Email"
+                  autoComplete="email"
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  required
+                  id="occupation"
+                  label="Occupation"
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  required
+                  id="dateOfBirth"
+                  type="date"
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  required
+                  id="password"
+                  type="password"
+                  label="Password"
+                  onChange={handleChange}
+                />
+              </Grid>
             </Grid>
 
-            <Grid item xs={12} sm={6}>
-              <TextField
-              fullWidth
-                required
-                id="surname"
-                label="Surname"
-                autoComplete="family-name"
-              />
-            </Grid>
-
-            <Grid item xs={12}>
-              <TextField
-              fullWidth
-                required
-                id="email"
-                label="Email"
-                autoComplete="email"
-              />
-            </Grid>
-
-            <Grid item xs={12}>
-              <TextField
-              fullWidth
-                required
-                id="occupation"
-                label="Occupation"
-              />
-            </Grid>
-
-            <Grid item xs={12} >
-              <TextField
-              fullWidth
-                required
-                id="dateOfBirth"
-                type="date"
-              />
-            </Grid>
-
-            <Grid item xs={12} >
-              <TextField
-              fullWidth
-                required
-                id="password"
-                type="password"
-                label="Password"
-              />
-            </Grid>
-          </Grid>
-
-          <Button
+            <Button
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+              sx={{
+                mt: 3,
+                mb: 2,
+                bgcolor: "#A4D8E7",
+                ":hover": { bgcolor: "#5B96CB" },
+              }}
             >
               Sign Up
             </Button>
+          </Box>
         </Box>
-      </Box>
-    </Container>
-  );
-};
+      </Container>
+    );
+  };
 
-export default LoginPage;
+  export default LoginPage;

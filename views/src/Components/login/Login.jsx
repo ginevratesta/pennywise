0bb00261327f47af7bbf4c2da02118db946c77d0
Ginevra Testa <ginevra99testaa@gmail.com>
2024-04-23 17:10:59 +0200

    logic-adjustments-plus-modal

--- views/src/Components/login/Login.jsx
+++ views/src/Components/login/Login.jsx
@@ -11,6 +11,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Animation from "../animation/Animation";
+import PasswordModal from "../modals/PasswordModal";
 import "./Login.css";
 
 const LoginPage = () => {
@@ -40,32 +41,32 @@ const LoginPage = () => {
     try {
       if (isUser) {
         await axios.post("http://localhost:3023/login", loginData);
+        navigate("/home");
       } else {
         await axios.post("http://localhost:3023/signup", formData);
         setSuccessAlert(true);
+        setTimeout(() => {
+          navigate("/home");
+        }, 1500);
       }
     } catch (error) {
       console.error("Error creating user:", error);
       setErrorAlert(true);
-    } finally {
-      if (isUser) {
-        setLoginData({
-          email: "",
-          password: "",
-        });
-      } else {
-        setFormData({
-          name: "",
-          surname: "",
-          email: "",
-          dateOfBirth: "",
-          occupation: "",
-          password: "",
-        });
-      }
-      setTimeout(() => {
-        navigate("/home");
-      }, 1500);
+    }
+    if (isUser) {
+      setLoginData({
+        email: "",
+        password: "",
+      });
+    } else {
+      setFormData({
+        name: "",
+        surname: "",
+        email: "",
+        dateOfBirth: "",
+        occupation: "",
+        password: "",
+      });
     }
   };
 
@@ -101,163 +102,170 @@ const LoginPage = () => {
     setTimeout(() => {
       setIsLoading(false);
     }, 2500);
-  }, [formData, loginData, isUser]);
+  }, [formData, loginData, isUser, errorAlert, successAlert]);
 
   return (
     <>
-    { isLoading ? (
-      <Animation />
-    ) : (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          py: "24px",
-          mb: "40px",
-          mt: "24px",
-        }}
-      >
-        <Typography variant="h4" m="0" pb="16px" color="#CA8EB4">
-          {isUser ? "Sign in" : "Sign up"}
-        </Typography>
-
-        <Box
-          onSubmit={handleSubmit}
-          component="form"
-          sx={{
-            border: 2,
-            borderColor: "#CA8EB4",
-            borderRadius: "16px",
-            boxShadow: 10,
-          }}
-          p="32px"
-        >
-          {successAlert && (
-            <Alert severity="success" onClose={() => setSuccessAlert(false)}>
-              Signup successful!
-            </Alert>
-          )}
-          {errorAlert && (
-            <Alert severity="error" onClose={() => setErrorAlert(false)}>
-              User already exists. Please sign in.
-            </Alert>
-          )}
-
-          <Grid container spacing={2}>
-            {!isUser && (
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  required
-                  id="name"
-                  label="Name"
-                  autoComplete="first-name"
-                  autoFocus
-                  onChange={handleChange}
-                  value={formData.name}
-                />
-              </Grid>
-            )}
-
-            {!isUser && (
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  required
-                  id="surname"
-                  label="Surname"
-                  autoComplete="family-name"
-                  onChange={handleChange}
-                  value={formData.surname}
-                />
-              </Grid>
-            )}
-
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                required
-                id="email"
-                label="Email"
-                autoComplete="email"
-                onChange={handleChange}
-                value={isUser ? loginData.email : formData.email}
-              />
-            </Grid>
-
-            {!isUser && (
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  required
-                  id="occupation"
-                  label="Occupation"
-                  onChange={handleChange}
-                  value={formData.occupation}
-                />
-              </Grid>
-            )}
-
-            {!isUser && (
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  required
-                  id="dateOfBirth"
-                  type="date"
-                  onChange={handleChange}
-                  value={formData.dateOfBirth}
-                />
-              </Grid>
-            )}
-
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                required
-                id="password"
-                type="password"
-                label="Password"
-                onChange={handleChange}
-                value={isUser ? loginData.password : formData.password}
-              />
-            </Grid>
-          </Grid>
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
+      {isLoading ? (
+        <Animation />
+      ) : (
+        <Container component="main" maxWidth="xs">
+          <Box
             sx={{
-              mt: 3,
-              mb: 2,
-              bgcolor: "#CA8EB4",
-              ":hover": { bgcolor: "#FFAD8E" },
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              py: "24px",
+              mb: "40px",
+              mt: "24px",
             }}
           >
-            {isUser ? "Sign in" : "Sign Up"}
-          </Button>
-          <Grid container>
-            <Grid item xs={12}>
-              <Typography
-                variant="subtitle2"
-                sx={{ textAlign: "right" }}
-                id="signin"
-                onClick={handleUser}
-                value={isUser}
+            <Typography variant="h4" m="0" pb="16px" color="#CA8EB4">
+              {isUser ? "Sign in" : "Sign up"}
+            </Typography>
+
+            <Box
+              onSubmit={handleSubmit}
+              component="form"
+              sx={{
+                border: 2,
+                borderColor: "#CA8EB4",
+                borderRadius: "16px",
+                boxShadow: 10,
+              }}
+              p="32px"
+            >
+              {successAlert && (
+                <Alert
+                  severity="success"
+                  onClose={() => setSuccessAlert(false)}
+                >
+                  Signup successful!
+                </Alert>
+              )}
+
+              <Grid container spacing={2}>
+                {!isUser && (
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="name"
+                      label="Name"
+                      autoComplete="first-name"
+                      autoFocus
+                      onChange={handleChange}
+                      value={formData.name}
+                    />
+                  </Grid>
+                )}
+
+                {!isUser && (
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="surname"
+                      label="Surname"
+                      autoComplete="family-name"
+                      onChange={handleChange}
+                      value={formData.surname}
+                    />
+                  </Grid>
+                )}
+
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    required
+                    id="email"
+                    label="Email"
+                    autoComplete="email"
+                    onChange={handleChange}
+                    value={isUser ? loginData.email : formData.email}
+                  />
+                </Grid>
+
+                {!isUser && (
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="occupation"
+                      label="Occupation"
+                      onChange={handleChange}
+                      value={formData.occupation}
+                    />
+                  </Grid>
+                )}
+
+                {!isUser && (
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="dateOfBirth"
+                      type="date"
+                      onChange={handleChange}
+                      value={formData.dateOfBirth}
+                    />
+                  </Grid>
+                )}
+
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    required
+                    id="password"
+                    type="password"
+                    label="Password"
+                    onChange={handleChange}
+                    value={isUser ? loginData.password : formData.password}
+                  />
+                  <PasswordModal />
+                </Grid>
+              </Grid>
+
+              {errorAlert && (
+                <Alert severity="error" onClose={() => setErrorAlert(false)}>
+                  {isUser
+                    ? "Wrong email or password"
+                    : "Either the user already exists or the password doesn't respect the policy rules."}
+                </Alert>
+              )}
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  bgcolor: "#CA8EB4",
+                  ":hover": { bgcolor: "#FFAD8E" },
+                }}
               >
-                {isUser
-                  ? "Don't have an account yet? Sign up"
-                  : "Already have an account? Sign in"}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-    </Container>
-    )}
+                {isUser ? "Sign in" : "Sign Up"}
+              </Button>
+              <Grid container>
+                <Grid item xs={12}>
+                  <Typography
+                    variant="subtitle2"
+                    sx={{ textAlign: "right" }}
+                    id="signin"
+                    onClick={handleUser}
+                    value={isUser}
+                  >
+                    {isUser
+                      ? "Don't have an account yet? Sign up"
+                      : "Already have an account? Sign in"}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      )}
     </>
   );
 };
---
+++ views/src/Components/modals/PasswordModal.jsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Slide,
+  Typography,
+} from "@mui/material";
+import "./PasswordModal.css";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const PasswordModal = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Typography
+        className="pointer"
+        variant="subtitle2"
+        color="#CA8EB4"
+        sx={{ ":hover": { color: "#FFAD8E" } }}
+        onClick={handleClickOpen}
+      >
+        Password Policy
+      </Typography>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle color="#FA98A8">{"PASSWORD POLICY RULES"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            <ul>
+              <li>
+                Passwords must be at least <span className="rules">8 characters</span> long
+                and no more than <span className="rules">12 characters</span>.
+              </li>
+              <li>
+                Passwords must contain at least{" "}
+                <span className="rules">one uppercase letter</span>.
+              </li>
+              <li>
+                Passwords must contain at least <span className="rules">one number</span>.
+              </li>
+              <li>
+                Passwords must contain at least{" "}
+                <span className="rules">one special character</span>.
+              </li>
+            </ul>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button sx={{color: "#FA98A8", ":hover": {bgcolor: "#fffee0", color:"#FFAD8E"}}} onClick={handleClose}>I understand</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+};
+
+export default PasswordModal;
